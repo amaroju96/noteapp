8a621ebfa58358dcb8dc5a968b2894656d4fdb5e
maktech96 <maroju.ajay47@gmail.com>
2020-11-14 12:03:26 +0530

    first commit

---
+++ src/backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+var cors = require('cors');
+const authRoute = require('./routes/authentication');
+const notesRoute = require('./routes/svaenotes');
+app.use(cors());
+
+
+// db connection
+var dburl = "mongodb+srv://Note:1234@cluster0.mpmpx.mongodb.net/<Notebook>?retryWrites=true&w=majority"
+mongoose.connect(dburl, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(success => {
+    console.log('Connected to database');
+})
+.catch(err => {
+    console.log(`Errpr in connectig db ${err}`);
+});
+
+
+//body parser
+app.use(express.json());
+app.use('/student', authRoute);
+app.use('/student', notesRoute);
+app.use('/teacher', notesRoute);
+const port = 8000;
+// assignin port number to server
+app.listen(process.env.PORT || port,()=>{
+    console.log("server is listening on ",port);
+    
+})
